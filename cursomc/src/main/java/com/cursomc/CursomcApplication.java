4f8aae03706b4b419f73ddb11f3676b00d888ae2
Willian Bampi <willian.bampi@gmail.com>
2020-03-05 19:24:40 -0300

    Created instantiation operation to categories when the application is started.
    Signed-off-by: Willian Bampi <willian.bampi@gmail.com>

--- cursomc/src/main/java/com/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cursomc.domain.Category;
+import com.cursomc.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		createCategories();
+	}
+
+	private void createCategories() {
+		Category category1 = new Category(null, "Informática");
+		Category category2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(category1, category2));
 	}
 
 }
