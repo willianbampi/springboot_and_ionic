492c828601422ab7607f6cdcfe93da6703c5df6f
Willian Bampi <willian.bampi@gmail.com>
2020-03-16 20:18:05 -0300

    Create smtp email service to send email by google server.
    Signed-off-by: Willian Bampi <willian.bampi@gmail.com>

--- cursomc/src/main/java/com/cursomc/config/DevConfig.java
+++ cursomc/src/main/java/com/cursomc/config/DevConfig.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.cursomc.services.EmailService;
+import com.cursomc.services.SmtpEmailService;
+
 @Configuration
 @Profile("dev")
 public class DevConfig {
@@ -27,6 +30,11 @@ public class DevConfig {
 		
 		databaseService.instantiateDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
