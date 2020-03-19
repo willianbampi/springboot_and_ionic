feb7557ed6fe26585832e961002cb34f8d98e62a
Willian Bampi <willian.bampi@gmail.com>
2020-03-19 15:46:19 -0300

    Create restriction to client recover only your own orders.
    Signed-off-by: Willian Bampi <willian.bampi@gmail.com>

--- cursomc/src/main/java/com/cursomc/resources/OrderResource.java
+++ cursomc/src/main/java/com/cursomc/resources/OrderResource.java
@@ -5,11 +5,13 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -34,6 +36,16 @@ public class OrderResource {
 		order = service.insert(order);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(order.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Page<Order>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction, 
+			@RequestParam(value = "orderBy", defaultValue = "date") String orderBy) {
+		Page<Order> pageOrder = service.findPage(page, linesPerPage, direction, orderBy);
+		return ResponseEntity.ok().body(pageOrder);
 	}
 
 }
--- cursomc/src/main/java/com/cursomc/services/OrderService.java
+++ cursomc/src/main/java/com/cursomc/services/OrderService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cursomc.domain.Client;
 import com.cursomc.domain.Order;
 import com.cursomc.domain.OrderItem;
 import com.cursomc.domain.SlipPayment;
@@ -14,6 +18,8 @@ import com.cursomc.domain.enums.PaymentStatus;
 import com.cursomc.repositories.OrderItemRepository;
 import com.cursomc.repositories.OrderRepository;
 import com.cursomc.repositories.PaymentRepository;
+import com.cursomc.security.UserSpringSecurity;
+import com.cursomc.services.exceptions.AuthorizationException;
 import com.cursomc.services.exceptions.ObjectNotFoundException;
 import com.cursomc.services.utils.SlipService;
 
@@ -77,5 +83,15 @@ public class OrderService {
 		
 		return order;
 	}
+	
+	public Page<Order> findPage(Integer page, Integer linesPerPage, String direction, String orderBy){
+		UserSpringSecurity userSpringSecurity = UserService.authenticated();
+		if(userSpringSecurity == null) {
+			throw new AuthorizationException("Access denied.");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Client client = clientService.findById(userSpringSecurity.getId());
+		return rep.findByClient(client, pageRequest);
+ 	}
 
 }
